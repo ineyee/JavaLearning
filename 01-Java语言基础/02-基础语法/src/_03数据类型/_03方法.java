176c6488d2a07c7f88561a9c1ded9977849fb3a4
ineyee <ineyee@foxmail.com>
2025-08-12 18:24:21 +0800

    temp

---
+++ 01-Java语言基础/02-基础语法/src/_03数据类型/_03方法.java
@@ -0,0 +1,15 @@
+package _03数据类型;
+
+/*
+ * 函数：函数是独立的代码块，不属于任何类或对象，可以通过函数名直接调用
+ * 方法：当函数定义在类内部时就是方法，必须通过类本身或类的实例来调用。方法的第一个参数通常都是隐藏参数 self（实例方法）或 cls（类方法），用于访问对象或类的数据，
+ * Java 里
+ */
+public class _03方法 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
