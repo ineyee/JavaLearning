e10184c2272240965c670dc2f1270820c66e3f82
ineyee <ineyee@foxmail.com>
2025-08-13 14:32:13 +0800

    Java语言基础：基础语法：枚举

--- 01-Java语言基础/02-基础语法/src/_03数据类型/_04枚举.java
+++ 01-Java语言基础/02-基础语法/src/_03数据类型/_04枚举.java
@@ -1,6 +1,16 @@
-package _01枚举;
+package _03数据类型;
 
-public class Main {
+/*
+ * 跟很多其它语言一样，如果一个变量的取值只可能是固定的几个值，可以考虑使用枚举类型，枚举由一组预定义的常量构成，所以全大写加_
+ */
+enum Season {
+	SPRING,
+	SUMMER,
+	AUTUMN,
+	WINTER,
+}
+
+public class _04枚举 {
 
 	public static void main(String[] args) {
 		Season season = Season.AUTUMN;
