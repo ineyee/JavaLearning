2373d6cd185e0631b0428bbe01dc9daf13bd57fc
ineyee <ineyee@foxmail.com>
2025-08-15 08:29:11 +0800

    Java 语言基础：面向对象：四大组成存储在哪里

---
+++ 01-Java语言基础/03-面向对象/src/_01_类_对象_成员变量_成员函数/_04_成员属性存储在哪里.java
@@ -0,0 +1,14 @@
+package _01_类_对象_成员变量_成员函数;
+
+public class _04_成员属性存储在哪里 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+ * 成员属性的值存储在对象内部，这也很好理解，不同对象的成员属性有不同的值，所以成员属性的值必须要存储在各自对象的内部，否则就没办法区分某个值到底是哪个对象的
+ */
---
+++ 01-Java语言基础/03-面向对象/src/_01_类_对象_成员变量_成员函数/_05_成员方法存储在哪里.java
@@ -0,0 +1,14 @@
+package _01_类_对象_成员变量_成员函数;
+
+public class _05_成员方法存储在哪里 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+ * 成员方法存储在代码区，这也很好理解，对于同一个成员方法所有的对象调用的都是同一段代码，所以成员成员只需要在代码区存一份就够了，没必要每个对象内部都存一份浪费内存。
+ */
