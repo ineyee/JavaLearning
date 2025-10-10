01d6d36e1574a4ebc46af31ede493848195309a9
ineyee <2487407179@qq.com>
2025-10-10 17:20:51 +0800

    JavaEE流行框架：MyBatis：删

--- 03-JavaEE流行框架/01-MyBatis/hello-my-batis/src/test/java/UserTest.java
+++ 03-JavaEE流行框架/01-MyBatis/hello-my-batis/src/test/java/UserTest.java
@@ -43,6 +43,28 @@ public class UserTest {
         }
     }
 
+    // 删除成功，一般是给客户端返回成功或失败的信息即可，客户端根据返回结果决定要不要更新内存数据就行
+    @Test
+    void remove() {
+        // 创建一个会话，项目运行期间可以创建多个
+        try (SqlSession session = MyBatisUtil.openSession()) {
+            // 执行 SQL 语句
+            // session.delete(statement, parameter)：用来删除数据，成功则返回影响数据条数，失败则返回 0，出错则抛异常
+            //   statement（String）：SQL 语句的命名空间.SQL 语句的唯一标识
+            //   parameter（Object）：SQL 语句的参数，当参数个数为一个时，直接把参数值传进去就可以了
+            int ret = session.delete("com.ineyee.dao.UserDao.remove", 31);
+            if (ret > 0) {
+                System.out.println("删除成功");
+            } else {
+                System.out.println("删除失败");
+            }
+
+            // MyBatis 默认开启了事务，关闭了自动提交
+            // 所以针对“增删改”操作，我们需要手动提交事务，“查”操作不影响数据库，不需要提交事务
+            session.commit();
+        }
+    }
+
     // 更新成功，一般是给客户端返回成功或失败的信息即可，客户端根据返回结果决定要不要更新内存数据就行
     @Test
     void update() {
@@ -51,14 +73,11 @@ public class UserTest {
             // 执行 SQL 语句
             // session.update(statement, parameter)：用来更新数据，成功则返回影响数据条数，失败则返回 0，出错则抛异常
             //   statement（String）：SQL 语句的命名空间.SQL 语句的唯一标识
-            //   parameter（Object）：SQL 语句的参数，当参数个数为一个时，直接把参数值传进去就可以了
-
-            // 这个 userBean 演示有的可选属性没值
-            UserBean userBean = new UserBean();
-            userBean.setId(31);
-            userBean.setAge(40);
-
-            int ret = session.update("com.ineyee.dao.UserDao.update", userBean);
+            //   parameter（Object）：SQL 语句的参数，当参数个数为多个时，可以搞成 bean 或 Map 把参数传进去（value 值在外界算好再传进去，里面只能读取、不能进行运算）
+            int ret = session.update("com.ineyee.dao.UserDao.update", Map.of(
+                    "id", 31,
+                    "name", "James"
+            ));
             if (ret > 0) {
                 System.out.println("更新成功");
             } else {
@@ -91,7 +110,7 @@ public class UserTest {
             // 执行 SQL 语句
             // session.selectList(statement, parameter)：用来查询多条数据，查到则返回 [bean]，查不到则返回 []
             //   statement（String）：SQL 语句的命名空间.SQL 语句的唯一标识
-            //   parameter（Object）：SQL 语句的参数，当参数个数为多个时，可以搞成 Map 把参数传进去（value 值在外界算好再传进去，里面只能读取、不能进行运算）
+            //   parameter（Object）：SQL 语句的参数，当参数个数为多个时，可以搞成 bean 或 Map 把参数传进去（value 值在外界算好再传进去，里面只能读取、不能进行运算）
             Integer pageSize = 2;
             Integer currentPage = 2;
             List<UserBean> userBeanList = session.selectList("com.ineyee.dao.UserDao.list", Map.of(
