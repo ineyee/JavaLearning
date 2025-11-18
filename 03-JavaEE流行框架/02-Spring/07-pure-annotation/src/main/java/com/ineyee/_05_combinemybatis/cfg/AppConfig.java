fe5054d8f0520ad288fdb31779f2f296f20144df
ineyee <2487407179@qq.com>
2025-11-18 18:03:38 +0800

    JavaEE流行框架：Spring：纯注解：纯注解 Spring 整合 MyBatis

---
+++ 03-JavaEE流行框架/02-Spring/07-pure-annotation/src/main/java/com/ineyee/_05_combinemybatis/cfg/AppConfig.java
@@ -0,0 +1,9 @@
+package com.ineyee._05_combinemybatis.cfg;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ineyee._05_combinemybatis")
+public class AppConfig {
+}
