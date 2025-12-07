1831dfaf6d0c58bba55b5bd6bbb4135291b9979f
ineyee <ineyee@foxmail.com>
2025-12-07 13:32:01 +0800

    JavaEE流行框架：SpringMVC：返回响应体

---
+++ 03-JavaEE流行框架/03-SpringMVC/07-response-data/src/main/java/com/ineyee/controller/TestController.java
@@ -0,0 +1,57 @@
+package com.ineyee.controller;
+
+import com.ineyee.domain.Dog;
+import com.ineyee.domain.Person;
+import com.ineyee.domain.Response;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class TestController {
+    // 响应体是普通文本
+    // 用 produces 告诉客户端响应体的格式为普通文本
+    // 用 @ResponseBody 注解修饰一下这个方法，这样一来 Spring 框架就会自动把该方法的返回值作为响应体返回给客户端并结束本次请求了
+    @GetMapping(value = "/testPlain", produces = "text/plain")
+    @ResponseBody
+    public String testPlain() {
+        return "<h1>登录成功<h1>";
+    }
+
+    // 响应体是 HTML 文本
+    // 用 produces 告诉客户端响应体的格式为 HTML 文本
+    // 用 @ResponseBody 注解修饰一下这个方法，这样一来 Spring 框架就会自动把该方法的返回值作为响应体返回给客户端并结束本次请求了
+    @GetMapping(value = "/testHtml", produces = "text/html")
+    @ResponseBody
+    public String testHtml() {
+        return "<h1>登录成功<h1>";
+    }
+
+    // 响应体是 JSON 字符串
+    // 用 produces 告诉客户端响应体的格式为 JSON 字符串
+    // 用 @ResponseBody 注解修饰一下这个方法，这样一来 Spring 框架就会自动把该方法的返回值作为响应体返回给客户端并结束本次请求了
+    // 只要项目里添加并配置了 jackson-databind 依赖，其余什么都不需要做，Spring 框架就会自动把该方法的返回值转为 JSON 字符串并返回给客户端了，因此我们只需要直接返回 Java 对象即可
+    @GetMapping(value = "/testJson", produces = "application/json")
+    @ResponseBody
+    public Response testJson() {
+        Dog dog1 = new Dog();
+        dog1.setName("旺财");
+        Dog dog2 = new Dog();
+        dog2.setName("二哈");
+
+        Person person = new Person();
+        person.setName("张三");
+        person.setAge(18);
+        person.setHeight(1.88);
+        person.setDogList(List.of(dog1, dog2));
+
+        Response response = new Response();
+        response.setCode(0);
+        response.setMessage("success");
+        response.setData(person);
+
+        return response;
+    }
+}
