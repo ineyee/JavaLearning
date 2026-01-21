8cc6566943887b8f7f11a6cc34345ea476015134
ineyee <2487407179@qq.com>
2026-01-21 15:58:16 +0800

    项目实战问题：跨域处理

---
+++ 04-项目实战问题/02-test-project/src/main/java/com/ineyee/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.ineyee.config;
+
+import com.ineyee.props.CorsProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 跨域处理配置
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Autowired
+    CorsProperties corsProperties;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // /**：代表对所有的请求进行跨域处理
+        registry.addMapping("/**")
+                // *：代表允许任意静态资源服务器的源跨域访问我们服务器上的接口
+                // 不过实际开发中我们一般就是填写自己静态资源服务器的源
+                .allowedOrigins(corsProperties.getAllowedOrigins())
+                // *：代表允许任意请求方法（GET、POST、HEAD、PUT、DELETE、CONNECT、OPTIONS、TRACE、PATCH）跨域访问
+                .allowedMethods("*")
+                // *：代表允许前端携带任意请求头
+                .allowedHeaders("*")
+                // true：代表允许前端跨域携带 Cookie
+                .allowCredentials(true);
+    }
+}
--- 04-项目实战问题/02-test-project/src/main/java/com/ineyee/config/MyBatisPlusConfig.java
+++ 04-项目实战问题/02-test-project/src/main/java/com/ineyee/config/MyBatisPlusConfig.java
@@ -6,12 +6,13 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// MyBatis-Plus 添加插件的配置
 @Configuration
 public class MyBatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        // 添加分页插件，指定数据库类型为 MySQL（如果配置多个插件, 切记分页最后添加）
+        // 添加分页插件拦截器，指定数据库类型为 MySQL（如果配置多个插件, 切记分页最后添加）
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
